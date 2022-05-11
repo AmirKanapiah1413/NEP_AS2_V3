9aac3fb839e52cac201af330c1c0859670c52a01
Hafiz Afiq <hafiz.afiq6079@gmail.com>
2022-05-12 00:21:26 +0800

    fixing navigation bar

--- src/main/java/com/example/nep/AS2/controller/HomeController.java
+++ src/main/java/com/example/nep/AS2/controller/HomeController.java
@@ -14,14 +14,28 @@ public class HomeController {
 	public String home() {
 	return "index";
 	}
-	
+	@RequestMapping(value="/index.html")
+	public String home2() {
+	return "index";
+	}	
 
-	@RequestMapping(value="/product")
+	@RequestMapping(value="/product.html")
 	public String product() {
 	return "product";
 	}
-
-
 	
+	@RequestMapping(value="/register_product.html")
+	public String register_product() {
+	return "register_product";
+	}
+
+	@RequestMapping(value="/search.html")
+	public String search() {
+	return "search";
+	}
 	
+	@RequestMapping(value="/support.html")
+	public String support() {
+	return "support";
+	}
 }
